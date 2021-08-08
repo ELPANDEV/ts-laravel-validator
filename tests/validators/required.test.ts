bbba50e73d3c41c8b963f29444ad869bd6e8883e
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 14:01:10 -0500

    .

---
+++ tests/validators/required.test.ts
@@ -0,0 +1,36 @@
+import IValues from "~/types/values";
+import validator_required from "~/validators/required";
+
+//#region true
+
+const permitted_values = [
+  'a',
+  'aasdasd',
+  1,
+  1231,
+  true,
+  false
+]
+
+permitted_values.forEach(value => {
+  test(`validator_required(${value}) to equal true`, () => {
+    expect(validator_required(value)).toEqual(true);
+  })
+})
+
+//#endregion
+//#region false
+
+const non_permitted_values = [
+  null,
+  undefined,
+  ''
+]
+
+non_permitted_values.forEach(value => {
+  test(`validator_required(${value}) to equal false`, () => {
+    expect(validator_required(value)).toEqual(false);
+  })
+})
+
+//#endregion
