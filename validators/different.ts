bc8290c321d64d3afbb9de68aada11542c70bc31
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 17:23:32 -0500

    different

--- validators/different.ts
+++ validators/different.ts
@@ -1,5 +1,7 @@
-const validator_different = (value: any): boolean => {
-  return true
+import IValues from "~/types/values"
+
+const validator_different = (value: any, rule_value: string, values: IValues): boolean => {
+  return values[rule_value] != value
 }
 
 export default validator_different
