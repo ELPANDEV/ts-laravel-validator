94746e706875f92183efc2e0235f903d80b5b294
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 17:12:29 -0500

    date_equals

--- validators/date_equals.ts
+++ validators/date_equals.ts
@@ -1,5 +1,7 @@
-const validator_date_equals = (value: any): boolean => {
-  return true
+import compare_date from "~/methods/compare_date"
+
+const validator_date_equals = (value: any, rule_value: string): boolean => {
+  return compare_date(value, '==', rule_value)
 }
 
 export default validator_date_equals
