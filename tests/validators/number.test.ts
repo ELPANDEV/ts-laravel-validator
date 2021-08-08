d46b1d913c8c98dd618890817f0791f2bda36238
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 14:06:13 -0500

    .

---
+++ tests/validators/number.test.ts
@@ -0,0 +1,40 @@
+import validator_number from "~/validators/number";
+
+//#region true
+
+const permitted_values = [
+  0,
+  1,
+  -1,
+  384738,
+  -38473
+]
+
+permitted_values.forEach(value => {
+  test(`validator_number(${value}) to equal true`, () => {
+    expect(validator_number(value)).toEqual(true);
+  })
+})
+
+//#endregion
+//#region false
+
+const non_permitted_values = [
+  'a',
+  '0',
+  '1',
+  '-1',
+  [],
+  true,
+  false,
+  {},
+  () => {}
+]
+
+non_permitted_values.forEach(value => {
+  test(`validator_number(${value}) to equal false`, () => {
+    expect(validator_number(value)).toEqual(false);
+  })
+})
+
+//#endregion
---
+++ tests/validators/string.test.ts
@@ -0,0 +1,47 @@
+import validator_string from "~/validators/string";
+
+//#region true
+
+const permitted_values = [
+  '',
+  'a',
+  'abcdefg',
+  '0',
+  '1',
+  '-1',
+  'true',
+  'false',
+  '[]',
+  '{}',
+  '() => {}',
+]
+
+permitted_values.forEach(value => {
+  test(`validator_string(${value}) to equal true`, () => {
+    expect(validator_string(value)).toEqual(true);
+  })
+})
+
+//#endregion
+//#region false
+
+const non_permitted_values = [
+  0,
+  1,
+  -1,
+  475487,
+  -37478934,
+  [],
+  true,
+  false,
+  {},
+  () => {}
+]
+
+non_permitted_values.forEach(value => {
+  test(`validator_string(${value}) to equal false`, () => {
+    expect(validator_string(value)).toEqual(false);
+  })
+})
+
+//#endregion
