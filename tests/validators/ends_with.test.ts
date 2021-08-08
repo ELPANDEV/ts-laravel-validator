128a872fe4fd06ceb673186aeee9018a372beb3c
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 17:36:47 -0500

    ends_with

---
+++ tests/validators/ends_with.test.ts
@@ -0,0 +1,37 @@
+import validator_ends_with from "~/validators/ends_with";
+
+type values = {
+  value: string,
+  end:   string
+}
+
+//#region true
+
+const permitted_values: values[] = [
+  { value: 'sjdjkejkjr',  end: 'kjr'      },
+  { value: 'asdlqeriej',  end: 'iej,ej'   },
+  { value: 'poiwertkjsd', end: 'jsd,sd,d' },
+]
+
+permitted_values.forEach(({ value, end }) => {
+  test(`validator_ends_with(${value}, ${end}) to equal true`, () => {
+    expect(validator_ends_with(value, end)).toEqual(true);
+  })
+})
+
+//#endregion
+//#region false
+
+const non_permitted_values = [
+  { value: 'sjdjkejkjr',  end: 'kj'    },
+  { value: 'asdlqeriej',  end: 'ri,as' },
+  { value: 'poiwertkjsd', end: 'js,we' },
+]
+
+non_permitted_values.forEach(({ value, end }) => {
+  test(`validator_ends_with(${value}, ${end}) to equal false`, () => {
+    expect(validator_ends_with(value, end)).toEqual(false);
+  })
+})
+
+//#endregion
--- validators/ends_with.ts
+++ validators/ends_with.ts
@@ -1,5 +1,9 @@
-const validator_ends_with = (value: any): boolean => {
-  return true
+const validator_ends_with = (value: any, rule_value: string): boolean => {
+  const values = rule_value.split(',')
+
+  return values.some(val => {
+    return value.match(new RegExp(`${val}$`)) ? true : false
+  })
 }
 
 export default validator_ends_with
