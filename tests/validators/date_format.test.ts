f215de8c15089fc44b8aeb2414c412d0932aa11b
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 17:19:47 -0500

    date_format

---
+++ tests/validators/date_format.test.ts
@@ -0,0 +1,38 @@
+import validator_date_format from "~/validators/date_format";
+
+type values = {
+  value:  string,
+  format: string
+}
+
+//#region true
+
+const permitted_values: values[] = [
+  { value: '10-10-2020', format: 'dd-mm-yyyy' },
+  { value: '2020-10-10', format: 'yyyy-mm-dd' },
+  { value: '10-2020-10', format: 'dd-yyyy-mm' },
+]
+
+permitted_values.forEach(({ value, format }) => {
+  test(`validator_date_format(${value}, ${format}) to equal true`, () => {
+    expect(validator_date_format(value, format)).toEqual(true);
+  })
+})
+
+//#endregion
+//#region false
+
+const non_permitted_values: values[] = [
+  { value: '10-10-20201', format: 'dd-mm-yyyy' },
+  { value: '2020-100-10', format: 'yyyy-mm-dd' },
+  { value: '1-2020-10',   format: 'dd-yyyy-mm' },
+  { value: '10-2020-1',   format: 'dd-yyyy-mm' },
+]
+
+non_permitted_values.forEach(({ value, format }) => {
+  test(`validator_date_format(${value}, ${format}) to equal false`, () => {
+    expect(validator_date_format(value, format)).toEqual(false);
+  })
+})
+
+//#endregion
--- validators/date_format.ts
+++ validators/date_format.ts
@@ -1,5 +1,12 @@
-const validator_date_format = (value: any): boolean => {
-  return true
+import validator_regex from "./regex"
+
+const validator_date_format = (value: any, rule_value: string): boolean => {
+  const regex = rule_value
+    .replace('dd',   '\\d{2}')
+    .replace('mm',   '\\d{2}')
+    .replace('yyyy', '\\d{4}')
+
+  return validator_regex(value, regex)
 }
 
 export default validator_date_format
