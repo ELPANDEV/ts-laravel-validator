cca3bcee108fd7156689340eeb2d4419ea04c37d
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 16:20:46 -0500

    .

--- validators/after_or_equal.ts
+++ validators/after_or_equal.ts
@@ -1,5 +1,7 @@
-const validator_after_or_equal = (value: any): boolean => {
-  return true
+import compare_date from "~/methods/compare_date"
+
+const validator_after_or_equal = (value: any, rule_value: string): boolean => {
+  return compare_date(value, '>=', rule_value)
 }
 
 export default validator_after_or_equal
--- validators/before_or_equal.ts
+++ validators/before_or_equal.ts
@@ -1,5 +1,7 @@
-const validator_before_or_equal = (value: any): boolean => {
-  return true
+import compare_date from "~/methods/compare_date"
+
+const validator_before_or_equal = (value: string, rule_value: string): boolean => {
+  return compare_date(value, '<=', rule_value)
 }
 
 export default validator_before_or_equal
