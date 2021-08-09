cafc4ed728a514ca5139ff5903b54c21307c7e06
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-09 09:27:38 -0500

    digits

---
+++ tests/validators/digits_between.test.ts
@@ -0,0 +1,41 @@
+import validator_digits_between from "~/validators/digits_between";
+
+type values = {
+  value:      string|number|any[]|File,
+  rule_value: string
+}
+
+//#region true
+
+const permitted_values: values[] = [
+  { value: 12345, rule_value: '5,10'  },
+  { value: 987,   rule_value: '2,10'  },
+  { value: 2546,  rule_value: '1,10'  },
+  { value: 27582, rule_value: '-1,10' },
+]
+
+permitted_values.forEach(({ value, rule_value }) => {
+  test(`validator_digits_between(${value}, ${rule_value}) to equal true`, () => {
+    expect(validator_digits_between(value, rule_value)).toEqual(true);
+  })
+})
+
+//#endregion
+//#region false
+
+const non_permitted_values: values[] = [
+  { value: '12345', rule_value: '5,10' },
+  { value: '987',   rule_value: '3,10' },
+  { value: 12345,   rule_value: '6,4'  },
+  { value: 987,     rule_value: '4,2'  },
+  { value: 2546,    rule_value: '-1,1' },
+  { value: 27582,   rule_value: '2,3'  },
+]
+
+non_permitted_values.forEach(({ value, rule_value }) => {
+  test(`validator_digits_between(${value}, ${rule_value}) to equal false`, () => {
+    expect(validator_digits_between(value, rule_value)).toEqual(false);
+  })
+})
+
+//#endregion
--- validators/digits_between.ts
+++ validators/digits_between.ts
@@ -1,5 +1,15 @@
-const validator_digits_between = (value: any): boolean => {
-  return true
+const validator_digits_between = (value: any, rule_value: string): boolean => {
+  if (typeof value == 'number') {
+    const length     = `${value}`.length 
+    const [min, max] = rule_value.split(',').map(e => parseInt(e))
+
+    return (
+      length >= min &&
+      length <= max
+    )
+  }
+  
+  return false
 }
 
 export default validator_digits_between
