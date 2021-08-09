532245fd914e7e0e60f50709000a82311b8cea56
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-08 22:31:44 -0500

    starts_with

---
+++ tests/validators/starts_with.test.ts
@@ -0,0 +1,37 @@
+import validator_starts_with from "~/validators/starts_with";
+
+type values = {
+  value: string,
+  start: string
+}
+
+//#region true
+
+const permitted_values: values[] = [
+  { value: 'sjdjkejkjr',  start: 'sjd'      },
+  { value: 'asdlqeriej',  start: 'asd,as'   },
+  { value: 'poiwertkjsd', start: 'poi,po,p' },
+]
+
+permitted_values.forEach(({ value, start }) => {
+  test(`validator_starts_with(${value}, ${start}) to equal true`, () => {
+    expect(validator_starts_with(value, start)).toEqual(true);
+  })
+})
+
+//#startregion
+//#region false
+
+const non_permitted_values = [
+  { value: 'sjdjkejkjr',  start: 'kj'       },
+  { value: 'asdlqeriej',  start: 'ri,dl'    },
+  { value: 'poiwertkjsd', start: 'js,we,sd' },
+]
+
+non_permitted_values.forEach(({ value, start }) => {
+  test(`validator_starts_with(${value}, ${start}) to equal false`, () => {
+    expect(validator_starts_with(value, start)).toEqual(false);
+  })
+})
+
+//#startregion
--- validators/starts_with.ts
+++ validators/starts_with.ts
@@ -1,5 +1,9 @@
-const validator_starts_with = (value: any): boolean => {
-  return true
+const validator_starts_with = (value: any, rule_value: string): boolean => {
+  const values = rule_value.split(',')
+
+  return values.some(val => {
+    return value.match(new RegExp(`^${val}`)) ? true : false
+  })
 }
 
 export default validator_starts_with
