cf56d64d43d0a3438f8bdface9c2f5df0ecdf0d6
Francisco Moncayo <javoavmystery@gmail.com>
2021-08-09 10:24:28 -0500

    not_in, not_regex

--- validators/not_in.ts
+++ validators/not_in.ts
@@ -1,5 +1,7 @@
-const validator_not_in = (value: any): boolean => {
-  return true
+const validator_not_in = (value: any, rule_value: string): boolean => {
+  const values = rule_value.split(',')
+
+  return !values.includes(value)
 }
 
 export default validator_not_in
--- validators/not_regex.ts
+++ validators/not_regex.ts
@@ -1,4 +1,10 @@
-const validator_not_regex = (value: any): boolean => {
+const validator_not_regex = (value: any, rule_value: string): boolean => {
+  if (typeof value == 'string') {
+    const match = value.match(new RegExp(rule_value))
+    
+    if (match) return match[0] != value
+  }
+
   return true
 }
 
